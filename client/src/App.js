00491b2fb05d528186c485b9591866bb41dc872d
Anatoly Tverdovsky <antverdovsky@gmail.com>
2019-06-15 14:18:28 -0700

    Create Pre-Authentication Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * Client app component.
+ */
+class App extends Component {
+  // The URL of the server for authenticating the client
+  static AUTHENTICATE_SPOTIFY_URL = "http://localhost:5000/login";
+
+  /**
+   * Instantiates the app state. The app state stores whether or not the user
+   * has authenticated with Spotify.
+   */
+  constructor() {
+    super();
+    const PARAMS = new URLSearchParams(window.location.search);
+
+    // Check if the user is authenticated from the URL arguments
+    this.isAuthenticated = PARAMS.get("authenticated") === "true";
+    console.log(`isAuthenticated? ${this.isAuthenticated}`);
+  }
+
+  /**
+   * Sets the page title.
+   */
+  componentDidMount() {
+    document.title = "Smart Spotify Playlists";
+  }
+
+  /**
+   * Renders the client app.
+   */
+  render() {
+    // If the user has authenticated, returns a new line.
+    // Otherwise, prompts the user to authenticate with Spotify.
+    const authenticateSpotify = this.isAuthenticated ? (
+      <br />
+    ) : (
+      <a
+        href={App.AUTHENTICATE_SPOTIFY_URL}
+        className="spotifyAuthenticationURL"
+      >
+        Connect with Spotify
+      </a>
+    );
+
+    return (
+      <div className="App">
+        {/* Renders the page statics (title & description) */}
+        <header className="header">
+          <h1 className="title">Smart Spotify Playlists</h1>
+          <p className="description">
+            Create Spotify playlists based on music rules
+            <br />
+            Inspired by iTunes Smart Playlists
+          </p>
+        </header>
+
+        {/* Renders the spotify authentication link, if necessary */}
+        <div className="spotifyAuthentication">{authenticateSpotify}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("constructor", () => {
+  it("should set isAuthenticated=true when param authenticated=true", () => {
+    jest
+      .spyOn(URLSearchParams.prototype, "get")
+      .mockImplementation(() => "true");
+
+    expect(new App().isAuthenticated).toBe(true);
+  });
+
+  it("should set isAuthenticated=false when param authenticated!=true", () => {
+    jest.spyOn(URLSearchParams.prototype, "get").mockImplementation(() => null);
+
+    expect(new App().isAuthenticated).toBe(false);
+  });
+});
+
+describe("componentDidMount", () => {
+  it("should set page title", () => {
+    shallow(<App />);
+    expect(document.title).toBe("Smart Spotify Playlists");
+  });
+});
+
+describe("render", () => {
+  it("should render app title and app description", () => {
+    let wrapper = shallow(<App />);
+    let header = wrapper.find("header");
+
+    expect(header.children().length).toBe(2);
+    expect(header.find(".title").exists()).toBe(true);
+    expect(header.find(".description").exists()).toBe(true);
+  });
+
+  describe("render authentication", () => {
+    it("should render connect to spotify when not authenticated", () => {
+      jest
+        .spyOn(URLSearchParams.prototype, "get")
+        .mockImplementation(() => "false");
+
+      expect(
+        shallow(<App />)
+          .find(".spotifyAuthenticationURL")
+          .exists()
+      ).toBe(true);
+    });
+
+    it("should render connected to spotify when authenticated", () => {
+      jest
+        .spyOn(URLSearchParams.prototype, "get")
+        .mockImplementation(() => "true");
+
+      expect(
+        shallow(<App />)
+          .find(".spotifyAuthenticationURL")
+          .exists()
+      ).toBe(false);
+    });
+  });
 });
